f65a2c109d04d54984975becb83d147a1a495c87
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2003-10-22 20:53:59 +0000

    Added stuff from common.h
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@144 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch2/emsa.rb
+++ ai-app-prog/src/ch2/emsa.rb
@@ -1,3 +1,20 @@
 #!/usr/local/bin/ruby
 
-puts "hi"
+class Member
+	def initialize
+		@solution=[]
+		@energy=0.0
+	end
+end
+
+class Emsa
+	MAX_LENGTH=30
+	INITIAL_TEMPERATURE=30.0
+	FINAL_TEMPERATURE=0.5
+	ALPHA=0.99
+	STEPS_PER_CHANGE=100
+end
+
+if __FILE__ == $0
+	puts "hi"
+end
