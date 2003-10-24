89ecf134cf68ebb26deb5e72063b7f8884a0b120
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2003-10-24 19:15:13 +0000

    Implemented args.  Ruby rocks.
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@164 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch10/bigram.rb
+++ ai-app-prog/src/ch10/bigram.rb
@@ -8,8 +8,19 @@ class Bigram
 	LAST_WORD=2
 	START_SYMBOL=0
 	END_SYMBOL=1
+	
+	def initialize(debug, filename)
+		@current_word = 2
+		@word_vector = Array.new(MAX_WORDS)
+		@bigram_array = Array.new(MAX_WORDS)
+		@sum_vector = Array.new(MAX_WORDS)
+	end
 end
 
 if __FILE__ == $0
-	puts "hi"
+		if ARGV.length < 2 or !ARGV.include?("-f")
+				puts "./bigram -f <filename> [-v]"
+				exit
+		end
+	Bigram.new(ARGV.include?("-v"), ARGV[ARGV.index("-f")+1])
 end
