0504ecac1c1c35c10e22f9c3fb21b6a0d59fa56d
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2003-10-24 15:12:24 +0000

    Just getting started
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@161 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch10/bigram.rb
+++ ai-app-prog/src/ch10/bigram.rb
@@ -1,5 +1,15 @@
 #!/usr/local/bin/ruby
 
+class Bigram
+	MAX_WORD_LEN=40
+	MAX_WORDS=1000
+	FIRST_WORD=0
+	MIDDLE_WORD=1
+	LAST_WORD=2
+	START_SYMBOL=0
+	END_SYMBOL=1
+end
+
 if __FILE__ == $0
 	puts "hi"
 end
