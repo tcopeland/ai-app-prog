c2572205f61d24ed31a0ce62ea7425da91020101
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2003-10-28 20:13:48 +0000

    DataFile was getting out of hand, moved it back in
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@171 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch10/bigram.rb
+++ ai-app-prog/src/ch10/bigram.rb
@@ -1,48 +1,5 @@
 #!/usr/local/bin/ruby
 
-class DataFile
-	def initialize(filename)
-		@bigram_array = Array.new(Bigram::MAX_WORDS, 0)
-		@word = Array.new(Bigram::MAX_WORDS+1)
-		index = 0
-		first = false
-		file = File.open(filename, "r")
-		file.each_byte {|byte|
-			if file.eof
-				if index > 0
-					word[index]=0
-					index += 1
-					load_word(word, Bigram::LAST_WORD)
-					index = 0
-				end
-			elsif byte == 10 or byte == 13 or byte.chr == ' '
-				if index > 0
-					word[index] = 0
-					index += 1
-					if first
-						first = false
-						load_word(word, Bigram::FIRST_WORD)
-					else
-						load_word(word, Bigram::MIDDLE_WORD)
-					end	
-					index = 0
-				end
-			elsif byte.chr == '.' or byte.chr == '?'
-				word[index] = 0
-				index += 1
-				load_word(word, Bigram::MIDDLE_WORD)
-				load_word(word, Bigram::LAST_WORD)
-				index = 0
-				first = true
-			else
-				if byte != 10 and byte.chr != ','
-					word[index] = byte.chr
-				end
-			end
-		}
-	end
-end
-
 class Bigram
 	MAX_WORD_LEN=40
 	MAX_WORDS=1000
@@ -52,13 +9,58 @@ class Bigram
 	START_SYMBOL=0
 	END_SYMBOL=1
 	
-	def initialize(debug, datafile)
+	def initialize(debug, filename)
 		@current_word = 2
 		@word_vector = Array.new(MAX_WORDS)
 		@sum_vector = Array.new(MAX_WORDS)
+		@@last_index = START_SYMBOL
 		
 		@word_vector[0] = "<START>"
 		@word_vector[1] = "<END>"
+
+		@bigram_array = Array.new(MAX_WORDS, 0)
+		@word = Array.new(MAX_WORDS+1)
+		index = 0
+		first = false
+		file = File.open(filename, "r")
+		file.each_byte {|byte|
+			if file.eof
+				if index > 0
+					word[index]=0
+					index += 1
+					load_word(word, LAST_WORD)
+					index = 0
+				end
+			elsif byte == 10 or byte == 13 or byte.chr == ' '
+				if index > 0
+					word[index] = 0
+					index += 1
+					if first
+						first = false
+						load_word(word, FIRST_WORD)
+					else
+						load_word(word, MIDDLE_WORD)
+					end	
+					index = 0
+				end
+			elsif byte.chr == '.' or byte.chr == '?'
+				word[index] = 0
+				index += 1
+				load_word(word, MIDDLE_WORD)
+				load_word(word, LAST_WORD)
+				index = 0
+				first = true
+			else
+				if byte != 10 and byte.chr != ','
+					word[index] = byte.chr
+				end
+			end
+		}
+	end
+
+	def load_word(word, order)
+		word_index = 0
+		
 	end
 end
 
@@ -67,5 +69,5 @@ if __FILE__ == $0
 				puts "./bigram -f <filename> [-v]"
 				exit
 		end
-	Bigram.new(ARGV.include?("-v"), DataFile.new(ARGV[ARGV.index("-f")+1]))
+	Bigram.new(ARGV.include?("-v"),ARGV[ARGV.index("-f")+1])
 end
