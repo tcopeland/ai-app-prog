34f2fa3482302a173e86b035fff41ab46278c0f5
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-03-10 21:03:45 +0000

    Added Elements and sample data
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@224 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch5/backprop.rb
+++ ai-app-prog/src/ch5/backprop.rb
@@ -1,22 +1,58 @@
 #!/usr/local/bin/ruby
 
+class Element
+	attr_reader :health, :knife, :gun, :enemy, :output_neurons
+	def initialize(health, knife, gun, enemy, output_neurons)
+		@health, @knife, @gun, @enemy, @output_neurons = health, knife, gun, enemy, output_neurons
+	end
+end
+
 class BackProp
 	INPUT_NEURONS = 4
 	HIDDEN_NEURONS = 3
 	OUTPUT_NEURONS = 4
 	RAND_MAX = 2147483647.0
+	MAX_SAMPLES = 18
 	def initialize
 		@wih = []
 		@who = []
+		@inputs = []
+		@samples = [
+  		Element.new(2.0, 0.0, 0.0, 0.0, [0.0, 0.0, 1.0, 0.0]),
+		  Element.new(2.0, 0.0, 0.0, 1.0, [0.0, 0.0, 1.0, 0.0]),
+		  Element.new(2.0, 0.0, 1.0, 1.0, [1.0, 0.0, 0.0, 0.0]),
+		  Element.new(2.0, 0.0, 1.0, 2.0, [1.0, 0.0, 0.0, 0.0]),
+		  Element.new(2.0, 1.0, 0.0, 2.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(2.0, 1.0, 0.0, 1.0, [1.0, 0.0, 0.0, 0.0]),
+  		Element.new(1.0, 0.0, 0.0, 0.0, [0.0, 0.0, 1.0, 0.0]),
+		  Element.new(1.0, 0.0, 0.0, 1.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(1.0, 0.0, 1.0, 1.0, [1.0, 0.0, 0.0, 0.0]),
+		  Element.new(1.0, 0.0, 1.0, 2.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(1.0, 1.0, 0.0, 2.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(1.0, 1.0, 0.0, 1.0, [0.0, 0.0, 0.0, 1.0]),
+  		Element.new(0.0, 0.0, 0.0, 0.0, [0.0, 0.0, 1.0, 0.0]),
+		  Element.new(0.0, 0.0, 0.0, 1.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(0.0, 0.0, 1.0, 1.0, [0.0, 0.0, 0.0, 1.0]),
+		  Element.new(0.0, 0.0, 1.0, 2.0, [0.0, 1.0, 0.0, 0.0]),
+		  Element.new(0.0, 1.0, 0.0, 2.0, [0.0, 1.0, 0.0, 0.0]),
+		  Element.new(0.0, 1.0, 0.0, 1.0, [0.0, 0.0, 0.0, 1.0])
+		]
 	end
 	def rand_weight
 		(rand(RAND_MAX) / RAND_MAX) - 0.5
 	end
 	def run
 		err=0.0
-		i,sample,iterations,sum=0
+		i,iterations,sum = 0
+		sample = 0
 		file = File.open("stats.txt", "w")
 		assign_random_weights
+		while true
+			if (sample + 1) == MAX_SAMPLES	
+				sample = 0	
+			end
+			@inputs[0] = @samples[sample].health
+		end
 		file.close			
 	end
 	def assign_random_weights
