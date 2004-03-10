8b54d824a1326a206c0d75af725f110f887a93d9
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-03-10 21:53:24 +0000

    Implemented feed_forward
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@226 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch5/backprop.rb
+++ ai-app-prog/src/ch5/backprop.rb
@@ -17,6 +17,9 @@ class BackProp
 		@wih = []
 		@who = []
 		@inputs = []
+		@target = []
+		@hidden = []
+		@actual = []
 		@samples = [
   		Element.new(2.0, 0.0, 0.0, 0.0, [0.0, 0.0, 1.0, 0.0]),
 		  Element.new(2.0, 0.0, 0.0, 1.0, [0.0, 0.0, 1.0, 0.0]),
@@ -43,29 +46,66 @@ class BackProp
 	end
 	def run
 		err=0.0
-		i,iterations,sum = 0
-		sample = 0
+		i=sample=iterations=sum=0
 		file = File.open("stats.txt", "w")
 		assign_random_weights
 		while true
 			if (sample + 1) == MAX_SAMPLES	
 				sample = 0	
+			else 
+				sample += 1
 			end
 			@inputs[0] = @samples[sample].health
+			@inputs[1] = @samples[sample].knife
+			@inputs[2] = @samples[sample].gun
+			@inputs[3] = @samples[sample].enemy
+
+			@target[0] = @samples[sample].output_neurons[0]
+			@target[1] = @samples[sample].output_neurons[1]
+			@target[2] = @samples[sample].output_neurons[2]
+			@target[3] = @samples[sample].output_neurons[3]
+
+			feed_forward
+			
 		end
 		file.close			
 	end
+	def sigmoid(val)
+		(1.0 / (1.0 + Math.exp(-val.to_f)))
+	end
+	def feed_forward
+		HIDDEN_NEURONS.times {|hid|	
+			sum = 0.0
+			INPUT_NEURONS.times {|input|
+				sum += @inputs[input] * @wih[input][hid]
+			}
+			@wih[INPUT_NEURONS] = [] if @wih[INPUT_NEURONS] == nil
+			@wih[INPUT_NEURONS][hid] = 0.0 if @wih[INPUT_NEURONS][hid] == nil
+			sum += @wih[INPUT_NEURONS][hid]
+			@hidden[hid] = sigmoid(sum)
+		}	
+		OUTPUT_NEURONS.times {|output|
+			sum = 0.0
+			HIDDEN_NEURONS.times {|hid|
+				sum += @hidden[hid] * @who[hid][output]
+			}
+			@who[HIDDEN_NEURONS] = [] if @who[HIDDEN_NEURONS] == nil
+			@who[HIDDEN_NEURONS][output] = 0.0 if @who[HIDDEN_NEURONS][output] == nil
+			sum += @who[HIDDEN_NEURONS][output]
+			@actual[output] = sigmoid(sum)
+		}
+	end
 	def assign_random_weights
 		INPUT_NEURONS.times {|input|
-			HIDDEN_NEURONS.times {|hidden|
+			HIDDEN_NEURONS.times {|hid|
 				@wih[input] = [] if @wih[input] == nil
-				@wih[input][hidden] = rand_weight
+				@wih[input][hid] = rand_weight
 			}
 		}
-		HIDDEN_NEURONS.times {|hidden|
+		HIDDEN_NEURONS.times {|hid|
 			OUTPUT_NEURONS.times {|output|
-				@who[hidden] = [] if @who[hidden] == nil
-				@who[hidden][output] = rand_weight
+				@who[hid] = [] if @who[hid] == nil
+				@who[hid][output] = rand_weight
 			}
 		}
 	end
