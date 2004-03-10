16e8276c657968717a8441b3610c811e9b866526
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-03-10 16:51:43 +0000

    Implemented assign_random_weights
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@223 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch5/backprop.rb
+++ ai-app-prog/src/ch5/backprop.rb
@@ -1,12 +1,37 @@
 #!/usr/local/bin/ruby
 
 class BackProp
+	INPUT_NEURONS = 4
+	HIDDEN_NEURONS = 3
+	OUTPUT_NEURONS = 4
+	RAND_MAX = 2147483647.0
+	def initialize
+		@wih = []
+		@who = []
+	end
+	def rand_weight
+		(rand(RAND_MAX) / RAND_MAX) - 0.5
+	end
 	def run
 		err=0.0
 		i,sample,iterations,sum=0
-		out = ""
 		file = File.open("stats.txt", "w")
+		assign_random_weights
 		file.close			
+	end
+	def assign_random_weights
+		INPUT_NEURONS.times {|input|
+			HIDDEN_NEURONS.times {|hidden|
+				@wih[input] = [] if @wih[input] == nil
+				@wih[input][hidden] = rand_weight
+			}
+		}
+		HIDDEN_NEURONS.times {|hidden|
+			OUTPUT_NEURONS.times {|output|
+				@who[hidden] = [] if @who[hidden] == nil
+				@who[hidden][output] = rand_weight
+			}
+		}
 	end
 end
 
