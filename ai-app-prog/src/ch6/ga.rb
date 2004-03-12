b48c02077b72b1c509e9573c49dd08e3991ac655
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-03-12 21:21:38 +0000

    Implemented init.c
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@244 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch6/ga.rb
+++ ai-app-prog/src/ch6/ga.rb
@@ -1,5 +1,48 @@
 #!/usr/local/bin/ruby
 
+class Population
+	attr_accessor :fitness, :prog_size, :program
+	def initialize(fitness)
+		@fitness = fitness
+		@prog_size = Genetic::MAX_PROGRAM-1
+		@program = []
+	end
+end
+
+class Instructions
+	DUP=0
+	SWAP=1
+	MUL=2
+	ADD=3
+	OVER=4
+	NOP=5
+	MAX_INSTRUCTIONS=NOP+1
+end
+
+class Genetic
+	MAX_PROGRAM=6
+	MAX_CHROMS=3000
+	MAX_INSTRUCTIONS=6
+	def initialize
+		@current_population=0
+		@populations = []
+	end
+	def run
+		init_population
+	end
+	def init_population	
+		(MAX_CHROMS-1).times {|x| init_member(x) }
+	end
+	def init_member(index)
+		@populations[@current_population] = [] if @populations[@current_population] == nil
+		@populations[@current_population][index] = Population.new(0.0)
+		(MAX_PROGRAM-1).times {
+			|x| @populations[@current_population][index].program[x] = rand(Instructions::MAX_INSTRUCTIONS)
+		}
+	end
+	
+end
+
 if __FILE__ == $0
-	puts "hi"
+	Genetic.new.run
 end
