88a956bac463ba94cc606c649872a17e0d65e191
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-05-21 18:45:25 +0000

    Working on perform_art1
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@298 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch3/adaptive_resonance_theory.rb
+++ ai-app-prog/src/ch3/adaptive_resonance_theory.rb
@@ -4,6 +4,18 @@ class Adaptive
 	MAX_ITEMS = 11
 	MAX_CUSTOMERS = 10
 	TOTAL_PROTOTYPE_VECTORS = 5
+	DATABASE = [
+        [ 0,   0,   0,   0,   0,   1,   0,   0,   1,   0,   0],
+        [ 0,   1,   0,   0,   0,   0,   0,   1,   0,   0,   1],
+        [ 0,   0,   0,   1,   0,   0,   1,   0,   0,   1,   0],
+        [ 0,   0,   0,   0,   1,   0,   0,   1,   0,   0,   1],
+        [ 1,   0,   0,   1,   0,   0,   0,   0,   0,   1,   0],
+        [ 0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   1],
+        [ 1,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0],
+        [ 0,   0,   1,   0,   0,   0,   0,   0,   1,   0,   0],
+        [ 0,   0,   0,   0,   1,   0,   0,   1,   0,   0,   0],
+        [ 0,   0,   1,   0,   0,   1,   0,   0,   1,   0,   0]	
+	]
 	def initialize
 		@prototype_vector	= Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
 		@prototype_vector.each {|x|
@@ -16,8 +28,23 @@ class Adaptive
 		@members = Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
 		@membership = Array.new(MAX_CUSTOMERS, -1)
 	end
+	def perform_art1
+		and_result = Array.new(MAX_ITEMS)
+		exit = false
+		while !exit
+			exit = true
+			0.upto(MAX_CUSTOMERS-1) {|index|
+				0.upto(TOTAL_PROTOTYPE_VECTORS-1) {|pvec|	
+					if !@members[pvec].nil?
+						vector_bitwise_and(and_result, DATABASE[index][0], @prototype_vector[pvec][0])
+					end
+				}
+			}
+		end
+	end
 end
 
 if __FILE__ == $0
-	a = Adaptive.new
+	a = Adaptive.new	
+	a.perform_art1
 end
