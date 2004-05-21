27b099d20402b350280fd766792243286a402d5b
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-05-21 18:32:26 +0000

    Done with initialize
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@297 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch3/adaptive_resonance_theory.rb
+++ ai-app-prog/src/ch3/adaptive_resonance_theory.rb
@@ -1,5 +1,23 @@
 #!/usr/local/bin/ruby
 
+class Adaptive
+	MAX_ITEMS = 11
+	MAX_CUSTOMERS = 10
+	TOTAL_PROTOTYPE_VECTORS = 5
+	def initialize
+		@prototype_vector	= Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
+		@prototype_vector.each {|x|
+			x = Array.new(MAX_ITEMS, 0)
+		}
+		@sum_vector	= Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
+		@sum_vector.each {|x|
+			x = Array.new(MAX_ITEMS, 0)
+		}
+		@members = Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
+		@membership = Array.new(MAX_CUSTOMERS, -1)
+	end
+end
+
 if __FILE__ == $0
-	puts "hi"
+	a = Adaptive.new
 end
