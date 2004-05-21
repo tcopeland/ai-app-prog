3d50a8e301b5b68a417fc7cf93253d10e3e78991
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-05-21 19:29:40 +0000

    Couple of utility methods, more perform_art1
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@299 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch3/adaptive_resonance_theory.rb
+++ ai-app-prog/src/ch3/adaptive_resonance_theory.rb
@@ -18,12 +18,12 @@ class Adaptive
 	]
 	def initialize
 		@prototype_vector	= Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
-		@prototype_vector.each {|x|
-			x = Array.new(MAX_ITEMS, 0)
+		@prototype_vector.each_index {|x|
+			@prototype_vector[x] = Array.new(MAX_ITEMS, 0)
 		}
 		@sum_vector	= Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
-		@sum_vector.each {|x|
-			x = Array.new(MAX_ITEMS, 0)
+		@sum_vector.each_index {|x|
+			@sum_vector[x] = Array.new(MAX_ITEMS, 0)
 		}
 		@members = Array.new(TOTAL_PROTOTYPE_VECTORS, 0)
 		@membership = Array.new(MAX_CUSTOMERS, -1)
@@ -31,16 +31,28 @@ class Adaptive
 	def perform_art1
 		and_result = Array.new(MAX_ITEMS)
 		exit = false
+		mag_pe = mag_p = mag_e = 0
 		while !exit
 			exit = true
 			0.upto(MAX_CUSTOMERS-1) {|index|
 				0.upto(TOTAL_PROTOTYPE_VECTORS-1) {|pvec|	
 					if !@members[pvec].nil?
-						vector_bitwise_and(and_result, DATABASE[index][0], @prototype_vector[pvec][0])
+						vector_bitwise_and(and_result, DATABASE[index], @prototype_vector[pvec])
+						mag_pe = vector_magnitude(and_result)
+						mag_p = vector_magnitude(@prototype_vector[pvec])
+						mag_e = vector_magnitude(DATABASE[index])
 					end
 				}
 			}
 		end
+	end
+	def vector_magnitude(v)
+		res = 0
+		v.each {|x| res += x }
+		res
+	end
+	def vector_bitwise_and(res, v, w)
+		0.upto(MAX_ITEMS-1) {|i| res[i] = v[i] & w[i] }
 	end
 end
 
