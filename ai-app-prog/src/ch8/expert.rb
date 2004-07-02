afae68ce3b103d5348ca9386965da93b5da221ad
tom <tom@70466a2f-c39c-4051-9387-cdb7ccdafb47>
2004-07-02 19:36:41 +0000

    Can parse a simple defrule with no facts.  I'm doing a sort of callback thingy, but perhaps the token array should just be passed from parser to parser....
    git-svn-id: http://ai-app-prog.rubyforge.org/svn/trunk@322 70466a2f-c39c-4051-9387-cdb7ccdafb47

--- ai-app-prog/src/ch8/expert.rb
+++ ai-app-prog/src/ch8/expert.rb
@@ -1,6 +1,10 @@
 #!/usr/local/bin/ruby
 
 class Rule
+	attr_reader :name
+	def initialize(name)
+		@name = name
+	end
 end
 
 class Fact
@@ -10,12 +14,25 @@ class FactParser
 end
 
 class Token
+	attr_reader :txt
 	def initialize(txt)
 		@txt = txt
+	end
+	def delim?
+		false
+	end
+	def defrule?
+		false
 	end
 end
 
 class DelimiterToken < Token
+	def delim?
+		true
+	end
+	def open?
+		@txt = "("
+	end
 end
 
 class CmdToken < Token
@@ -34,7 +51,7 @@ class Tokenizer
 		@cur = ""
 		@discarding = false
 	end
-	def parse(txt)
+	def tokenize(txt)
 		txt.each_byte {|x|
 			c = x.chr
 			if @discarding
@@ -52,6 +69,7 @@ class Tokenizer
 			end
 		}
 		shift_accumulator if !@cur.empty?	
+		@tokens
 	end
 	private
 	def shift_accumulator
@@ -60,8 +78,70 @@ class Tokenizer
 	end
 end
 
+class RulesetParser
+	def initialize
+		@waiting_for_initial_delim = true
+	end
+	def handle(ctx, tok)
+		if @waiting_for_initial_delim
+			if tok.delim?
+				@waiting_for_initial_delim = false
+				return
+			end
+			raise "Expected initial delimiter!"
+		end
+		if tok.defrule?
+			@waiting_for_name = true
+		elsif tok.delim?
+			if tok.open?
+				ctx.push(RuleParser.new)
+			else
+				ctx.pop
+			end
+		else
+			ctx.rules << Rule.new(tok.txt)
+			@waiting_for_name = false
+		end
+	end
+end
+
+class RuleParser
+end
+
+class FactParser
+end	
+
+class Ctx
+	attr_reader :rules
+	def initialize
+		@rules = []
+		@parser_stack = [RulesetParser.new]
+	end
+	def push(s)
+		@parser_stack << s
+	end
+	def pop
+		@parser_stack.delete_at(@parser_stack.size-1)
+	end
+	def peek
+		@parser_stack.last
+	end
+end
+
+class Parser
+	attr_reader :rules
+	def initialize(input)
+		ctx = Context.new
+		Tokenizer.new.tokenize(input.kind_of?(File) ? File.read(input) : input).each {|t|
+			ctx.peek.handle(ctx, t)	
+		}
+	end
+end
+
+
 class Expert
 	def initialize(input)
+		p = Parser.new(input)		
 	end
 end
 
--- ai-app-prog/src/ch8/test_expert.rb
+++ ai-app-prog/src/ch8/test_expert.rb
@@ -6,25 +6,40 @@ require 'expert.rb'
 class TokenizerTest < Test::Unit::TestCase
 	def test_tokenize_delimiters
 		t = Tokenizer.new
-		t.parse("(")
+		t.tokenize("(")
 		assert(t.tokens.size == 1, "#{t.tokens.size} tokens, should be 1")
 	end
 	def test_discard_ws
 		t = Tokenizer.new
-		t.parse("   (  \n\r\t  \t  ")
+		t.tokenize("   (  \n\r\t  \t  ")
 		assert(t.tokens.size == 1, "#{t.tokens.size} tokens, should be 1")
 	end
 	def test_command
 		t = Tokenizer.new
-		t.parse("(defrule")
+		t.tokenize("(defrule")
 		assert(t.tokens.size == 2)
 		assert(t.tokens[1].kind_of?(CmdToken))
 		assert(t.tokens[1].defrule?)
 	end
 	def test_discard_comments
 		t = Tokenizer.new
-		t.parse("; this is a comment, followed by a defrule   \n (defrule")
+		t.tokenize("; this is a comment, followed by a defrule   \n (defrule")
 		assert(t.tokens.size == 2)
+	end
+end
+
+class RulesetParserTest < Test::Unit::TestCase
+	def test_simple
+		t = Tokenizer.new
+		t.tokenize("(defrule rulename)")
+		p = RulesetParser.new
+		c = Ctx.new
+		p.handle(c, t.tokens[0])
+		p.handle(c, t.tokens[1])
+		p.handle(c, t.tokens[2])
+		p.handle(c, t.tokens[3])
+		assert(c.rules.size == 1, "Should have parsed out 1 rule but parsed out #{c.rules.size}")
+		assert(c.rules[0].name == "rulename", "Rule name should be 'rulename' but is #{c.rules[0].name}")
 	end
 end
 
